34d8fdd200bc324a98fcf864736b49ce0305fab1
jisoodal <33686837+jisoodal@users.noreply.github.com>
2021-09-24 13:05:45 +0900

    initial commit

---
+++ aop-prac/src/main/java/com/aop/prac/AopPracApplication.java
@@ -0,0 +1,13 @@
+package com.aop.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopPracApplication.class, args);
+	}
+
+}
---
+++ aop-prac/src/test/java/com/aop/prac/AopPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aop.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
